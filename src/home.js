0b1b8f5777d80534ffa6336adddd706ae22f189e
Emilie Thon <emilie.m.thon@gmail.com>
2025-04-22 13:25:48 +0200

    colors added

--- src/home.js
+++
@@ -1,7 +0,0 @@
-import './style/style.scss'
-
-document.querySelector('#app').innerHTML = `
-  <h1>yo, man!</h1>
-  
-`
-
--- src/js/home/main.js
+++ src/js/home/main.js
@@ -5,7 +5,20 @@ export default function main() {
   sectionElm.className = "mainSection";
 
   sectionElm.innerHTML = `
-        
+    <h1>header</h1>
+    <ul>
+        <li>
+            <div>
+                <img src="${logo}" alt="Newslify logo" />  
+                <h2>Europe</h2>
+                <form action="#" class="switch" method="post">
+                    <input type="checkbox" id="switch" value="false" name="switch">
+                    <span class="slider round"></span>
+                </form>
+            </div>
+            
+        </li>
+    </ul>
         
     `;
 
