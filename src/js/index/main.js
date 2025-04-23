7ae1730ffd14909e6aa95b9f21f13f82b516b04a
Emilie Thon <emilie.m.thon@gmail.com>
2025-04-23 11:24:40 +0200

    sign-in style

--- src/js/index/main.js
+++ src/js/index/main.js
@@ -7,12 +7,16 @@ export default function main() {
   sectionElm.innerHTML = `
     <img src="${logo}" alt="Newslify logo" />  
     <h1>Newsify</h1>
-    <p>Welcome! Let's dive into your account</p>
-    <a class="btn1">Continue with Facebook</a>
-    <a class="btn1">Continue with Google</a>
-
-    <a class="btn2">Sign in with password</a>
-    <p>Don't have an account? <a href="./src/views/home.html">Sign up</a></p>
+    <p class="mainSection__p">Welcome! Let's dive into your account!</p>
+    <a href="./src/views/home.html" class="btn1">Continue with Facebook</a>
+    <a href="./src/views/home.html" class="btn1">Continue with Google</a>
+    <div>
+        <span></span>
+        <p>or</p>
+        <span></span>
+    </div>
+    <a href="./src/views/home.html" class="btn2">Sign in with password</a>
+    <p class="mainSection__p">Don't have an account? <a href="./src/views/home.html">Sign up</a></p>
     
         
     `;
